2a8319ed9e29937f61c0148acfc1d93caf6b4766
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-08-28 12:11:17 +0300

    update tests to match contract's code

--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -1,39 +1,56 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from "@ton/core";
 
+export type MainContractConfig = {
+  number: number;
+  address: Address;
+};
+
+export function mainContractConfigToCell(config: MainContractConfig) {
+    return beginCell().storeUint(config.number, 32).storeAddress(config.address).endCell();
+}
+
 export class MainContract implements Contract {
     constructor (
         readonly address: Address,
         readonly init?: { code: Cell, data: Cell }
     ) {}
 
-    static createFromConfig (config: any, code: Cell, workchain = 0) {
-        const data = beginCell().endCell();
+    static createFromConfig (config: MainContractConfig, code: Cell, workchain = 0) {
+        const data = mainContractConfigToCell(config);
         const init = { code, data };
         const address = contractAddress(workchain, init);
 
         return new MainContract(address, init);
     }
 
-    async sendInternalMessage(
+    async sendIncrement(
         provider: ContractProvider,
         sender: Sender,
-        value: bigint
+        value: bigint,
+        increment_by: number
     ) {
+        const opcode = 1;
+        const msg_body = beginCell()
+            .storeUint(opcode, 32)
+            .storeUint(increment_by, 32)
+        .endCell();
+
         await provider.internal(
             sender,
             {
                 value: value,
                 sendMode: SendMode.PAY_GAS_SEPARATELY,
-                body: beginCell().endCell()
+                body: msg_body
             }
         );
     }
 
     async getData(provider: ContractProvider) {
-        const { stack } = await provider.get("get_the_latest_sender", []);
-
-        return {
+        const { stack } = await provider.get("get_contract_storage_data", []);
+        const data = {
+            number: stack.readNumber(),
             recent_sender: stack.readAddress(),
         };
+        return data;
     }
 }
